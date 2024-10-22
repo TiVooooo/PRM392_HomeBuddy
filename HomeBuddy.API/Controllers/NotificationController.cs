af27d4239c8ad908cf56c8c7020ce7c2ac0e164f
tiivoooo <tienv5995@gmail.com>
2024-10-22 14:49:15 +0700

    HHHHHHHHHHH

---
+++ HomeBuddy.API/Controllers/NotificationController.cs
@@ -0,0 +1,23 @@
+﻿using HomeBuddy.Service.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeBuddy.API.Controllers
+{
+    public class NotificationController : ControllerBase
+    {
+        private readonly INotificationService _notificationService;
+
+        public NotificationController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        [HttpPost("send-notification")]
+        public async Task<IActionResult> SendNotification(string token, string title, string body)
+        {
+            var response = await _notificationService.SendNotification(token, title, body);
+            return Ok(response);
+        }
+    }
+
+}
--- HomeBuddy.Service/Services/NotificationService.cs
+++ HomeBuddy.Service/Services/NotificationService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FirebaseAdmin.Messaging;
+using HomeBuddy.Data.UnitOfWork;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +11,34 @@ namespace HomeBuddy.Service.Services
 
     public interface INotificationService
     {
+        Task<string> SendNotification(string token, string title, string body);
 
     }
 
     public class NotificationService : INotificationService
     {
+        private readonly UnitOfWork _unitOfWork;
+        public NotificationService(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<string> SendNotification(string token, string title, string body)
+        {
+            var message = new Message()
+            {
+                Token = token,
+                Notification = new Notification()
+                {
+                    Title = title,
+                    Body = body
+                },
+                Data = new Dictionary<string, string>()
+                {
+                    { "key1", "value1" }
+                }
+            };
+            string response = await FirebaseMessaging.DefaultInstance.SendAsync(message);
+            return response;
+        }
     }
 }
